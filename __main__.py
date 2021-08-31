f0c0fc9d9e964ecb0ca1d34f00706ba8765dd510
crinitusfeles <crinitusfeles@gmail.com>
2021-08-31 15:34:02 +0700

    fix: added semantic releases

--- __main__.py
+++ __main__.py
@@ -4,7 +4,7 @@ import sys
 # стандартные модули для работы
 
 # модули ОАИ_КПА
-from oai_kpa_stm import ClientGUIWindow
+from oai_kpa_mpp import ClientGUIWindow
 
 if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
     app = QtWidgets.QApplication(sys.argv)
